3a703c9ac6f3fd033446f4f10b07bae617480677
SadCunt <92012358+Nero490@users.noreply.github.com>
2021-12-13 13:11:53 +0100

    Add files via upload

---
+++ Debugger/Debugger/src/ejerciosDePrueba/Factorial.java
@@ -0,0 +1,22 @@
+package ejerciosDePrueba;
+
+import Console;
+
+public class Factorial {
+	public static void main(String[] args) {
+		
+		System.out.println("Dame un número");
+		int numero = Console.readInt();
+			int factorial = 1;
+			Int numeroFact = numero;
+		
+				while (numeroFact = 0) {
+					factorial = factorial*numeroFact;		
+					numeroFact--;
+				}
+				
+			System.out.println(El factorial es + factorial);
+		    numero = Console.readInt;
+		
+	}
+}
